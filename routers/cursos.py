203b84a3594ba52d36631ab2e311b17e5e232968
Jessica Trindade Nascimento de Brito <141635580+jessicatrindadeads@users.noreply.github.com>
2026-08-18 17:50:30 -0300

    chore: aprimora routers/cursos.py

--- routers/cursos.py
+++ routers/cursos.py
@@ -10,15 +10,15 @@ cursos_router = APIRouter()
 @cursos_router.get("/cursos", response_model=List[Curso])
 def read_cursos(db: Session = Depends(get_db)):
     cursos = db.query(ModelCurso).all()
-    return [Curso.from_orm(curso) for curso in cursos]
+    return [Curso.model_validate(curso) for curso in cursos]
 
 @cursos_router.post("/cursos", response_model=Curso)
 def create_curso(curso: Curso, db: Session = Depends(get_db)):
-    db_curso = ModelCurso(**curso.dict(exclude={"id"}))
+    db_curso = ModelCurso(**curso.model_dump())
     db.add(db_curso)
     db.commit()
     db.refresh(db_curso)
-    return Curso.from_orm(db_curso)
+    return Curso.model_validate(db_curso)
 
 @cursos_router.put("/cursos/{codigo_curso}", response_model=Curso)
 def update_curso(codigo_curso: str, curso: Curso, db: Session = Depends(get_db)):
@@ -26,39 +26,16 @@ def update_curso(codigo_curso: str, curso: Curso, db: Session = Depends(get_db))
     if db_curso is None:
         raise HTTPException(status_code=404, detail="Curso não encontrado")
 
-    for key, value in curso.dict(exclude_unset=True, exclude={"id"}).items():
+    for key, value in curso.model_dump(exclude_unset=True).items():
         setattr(db_curso, key, value)
 
     db.commit()
     db.refresh(db_curso)
-    return Curso.from_orm(db_curso)
+    return Curso.model_validate(db_curso)
 
 @cursos_router.get("/cursos/{codigo_curso}", response_model=Curso)
 def read_curso_por_codigo(codigo_curso: str, db: Session = Depends(get_db)):
     db_curso = db.query(ModelCurso).filter(ModelCurso.codigo == codigo_curso).first()
     if db_curso is None:
         raise HTTPException(status_code=404, detail="Nenhum curso encontrado com esse código")
-    return Curso.from_orm(db_curso)
-
-
-# Não buscar um curso pelo ID nem deletar em nenhuma hipótese
-
-# @cursos_router.get("/cursos/{curso_id}", response_model=Curso)
-# def read_curso(curso_id: int, db: Session = Depends(get_db)):
-#     db_curso = db.query(ModelCurso).filter(ModelCurso.id == curso_id).first()
-#     if db_curso is None:
-#         raise HTTPException(status_code=404, detail="Curso não encontrado")
-#     return Curso.from_orm(db_curso)
-
-
-# @cursos_router.delete("/cursos/{curso_id}", response_model=Curso)
-# def delete_curso(curso_id: int, db: Session = Depends(get_db)):
-#     db_curso = db.query(ModelCurso).filter(ModelCurso.id == curso_id).first()
-#     if db_curso is None:
-#         raise HTTPException(status_code=404, detail="Curso não encontrado")
-
-#     curso_deletado = Curso.from_orm(db_curso)
-
-#     db.delete(db_curso)
-#     db.commit()
-#     return curso_deletado
+    return Curso.model_validate(db_curso)
